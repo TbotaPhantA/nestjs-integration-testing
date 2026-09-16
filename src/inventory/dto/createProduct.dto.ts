9108233f2ebeb7ed46d098125104850eed75a5dc
TbotaPhantA <botakiller@mail.ru>
2026-09-16 21:08:13 +0000

    validation was added

--- src/inventory/dto/createProduct.dto.ts
+++ src/inventory/dto/createProduct.dto.ts
@@ -1,5 +1,18 @@
+import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
+import { MAX_INT_32 } from '../../shared/constants/maxInt32.js';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   name: string
+
+  @IsNotEmpty()
+  @IsString()
   description: string
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(MAX_INT_32)
   quantity: number
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
   setupSwagger();
   await app.listen(config.port);
 
