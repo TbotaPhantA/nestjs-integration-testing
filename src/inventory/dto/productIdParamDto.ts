d9f4801bef50cdf9e2237df271def1d58eceb2ba
TbotaPhantA <botakiller@mail.ru>
2026-09-18 23:57:40 +0000

    transform was added

--- src/inventory/dto/productIdParamDto.ts
+++ src/inventory/dto/productIdParamDto.ts
@@ -1,10 +1,12 @@
 import { IsInt, IsNotEmpty, Max, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ProductIdParamDto {
   @IsNotEmpty()
   @IsInt()
   @Min(1)
   @ApiProperty()
+  @Type(() => Number)
   productId: number
 }
