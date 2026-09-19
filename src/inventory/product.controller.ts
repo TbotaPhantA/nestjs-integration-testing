e0aa1647da805d10ae9e9b0dfa3b6b39a5ced86f
TbotaPhantA <botakiller@mail.ru>
2026-09-19 21:41:15 +0000

    revert

--- src/inventory/product.controller.ts
+++ src/inventory/product.controller.ts
@@ -6,7 +6,7 @@ import { ReDescribeProductDto } from './dto/reDescribeProduct.dto.js';
 import { ChangeQuantityDto } from './dto/changeQuantity.dto.js';
 import { ProductIdParamDto } from './dto/productIdParamDto.js';
 
-@Controller('inventory')
+@Controller('products')
 export class ProductController {
   constructor(
     private readonly service: ProductService,
