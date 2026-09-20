bc7c0b76e41954a1473b7580fd942de997662b22
TbotaPhantA <botakiller@mail.ru>
2026-09-20 01:03:33 +0000

    omit method was added to injection builder

--- test/shared/utils/injectionBuilder.ts
+++ test/shared/utils/injectionBuilder.ts
@@ -4,11 +4,23 @@ export class InjectionBuilder<T extends {} = {}> {
   constructor(
     private _target: T,
   ) {
-    this.result = _target
+    this.result = _target;
   }
 
   with<F extends keyof T>(field: Pick<T, F>): InjectionBuilder<T> {
-    Object.assign<T, Pick<T, F>>(this._target, field)
-    return this
+    Object.assign<T, Pick<T, F>>(this._target, field);
+    return this;
+  }
+
+  omit<K extends keyof T>(...keys: K[]): InjectionBuilder<Omit<T, K>> {
+    const updatedTarget = Object.create(Object.getPrototypeOf(this._target));
+
+    Object.assign(updatedTarget, this._target);
+
+    for (const key of keys) {
+      delete (updatedTarget as any)[key];
+    }
+
+    return new InjectionBuilder(updatedTarget as Omit<T, K>);
   }
 }
